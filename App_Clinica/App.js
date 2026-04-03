7bec26d1cb4f11ead5d89032215cdf8fb2a10d0f
ErkSansil <157431908+ErkSansil@users.noreply.github.com>
2026-04-02 21:08:44 -0300

    Add files via upload

---
+++ App_Clinica/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import { Text, View} from 'react-native';
+import { NavigationContainer } from
+'react-native';
+
+export default function App(){
+    return (
+        <View>
+            <Text>Clinica Mental</Text>
+        </View>
+    );
+}
+
+/* Instalar um import/npx que  eu consiga visualizar o código.
+
