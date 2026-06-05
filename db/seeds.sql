919d551a696ca048a2be7daf7fe1f22584fb8d53
GabrielRC-prog <gabrielruizcbs@gmail.com>
2026-06-05 19:31:35 -0300

    esquema banco de dados

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO sala (id, descricao) VALUES
+    ('83746291054', 'geral 1'),
+    ('10938475621', 'geral 2'),
+    ('57483920183', 'geral 3'),
+    ('29384756102', 'geral 4'),
+    ('48572910384', 'infantil'),
+    ('92837465019', 'grupo'),
+    ('61029384756', 'supervisao');
